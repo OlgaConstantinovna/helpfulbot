499008861bcc18400b0104b9541469b1afa5af8c
Olga <princessca.olga@icloud.com>
2023-02-11 22:14:28 +0300

    first

---
+++ helpfulbot.py
@@ -0,0 +1,22 @@
+import telebot
+
+token = "5888104101:AAGJrGV1UH2ky0DmqkxicD-k5lixo8D1FAw"
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Привет. Я бот техподдержки. Введите команду /question, чтобы задать свой вопрос")
+
+@bot.message_handler(commands=['question'])   
+def prepare_to_question(message):
+    bot.reply_to(message, 'Задайте свой вопрос 1 сообщением я предам его опретору')
+    bot.register_step(message,write_question())
+
+def write_question(message):
+	data = open('question.txt', 'a', encoding='utf-8')
+	data.write(f'{message.from_user.id}%%{message.from_user.firs_name}: {message.text}')
+	data.close()
+	bot.reply_to(message, 'Ваш вопрос отправлен оератору. Среднее время ожидания ответа 2 часа')
+
+
+bot.infinity_polling()
